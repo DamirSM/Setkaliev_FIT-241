bdde6796e711439661353a63833de0fadc342608
DAMIR <125278476+DamirSM@users.noreply.github.com>
2024-09-26 18:55:24 +0600

    Add files via upload

---
+++ Семестр 1/Лабораторная работа 1 20_09_2024/Задание 2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2_setkaliev_20_09_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Нахождение наибольшего и наименьшего значений, используя класс Math
+            Console.Write("Введите переменную 1: ");
+            int var1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите переменную 2: ");
+            int var2 = Convert.ToInt32(Console.ReadLine());
+            int max;
+            int min;
+
+            max = (var1 + var2 + Math.Abs(var1 - var2))/2;
+            min = (var1 + var2 - Math.Abs(var1 - var2))/2;
+
+            Console.WriteLine($"Максимальное значение - {max}\nМинимальное значение - {min}");
+        }
+    }
+}
---
+++ Семестр 1/Лабораторная работа 1 20_09_2024/Задание 3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3_setkaliev_20_09_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вычисление цикличного пути при помощи формул арифметической прогресии
+            // Входные данные для проверки: l = 3, m = 3, p = 5
+            // Результаты: n = 1 - 22, n = 2 - 50, n = 3 - 84, n = 4 - 124, n = 5 - 490
+            Console.Write("Введите длину грядки: ");
+            int l = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите ширину грядки: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите расстояние до колодца: ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество грядок: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum;
+
+            sum = 2 * n * (p + m) + (((2 * 2 * l + (n - 1) * 2 * l) / 2) * n);
+
+            Console.WriteLine($"Длина пути фермера: {sum}");
+        }
+    }
+}
