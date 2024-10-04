7c82c2708f0a05e27a606e8252038d5d6758b2f0
DAMIR <125278476+DamirSM@users.noreply.github.com>
2024-10-04 18:46:12 +0600

    Add files via upload

---
+++ Семестр 1/Лабораторные работы/Лабораторная работа 3 04_10_2024/Задание 2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2_setkaliev_04_10_2024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Определение максимального размера подпоследовательности,
+            // состоящей из одинаковых чётных элементов,
+            // последовательности из n элементов
+            Console.Write("Определение максимального размера подпоследовательности, " +
+                "состоящей из одинаковых чётных элементов" + 
+                "\nВведите количество элементов: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int count = 1;
+            int maxcount = int.MinValue;
+            Console.Write("Введите элемент: ");
+            int prevnum = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                Console.Write("Введите элемент: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                if ((num == prevnum) && (num % 2 == 0)) count++;
+                else
+                {
+                    if ((maxcount < count) && (count != 1)) maxcount = count;
+                    count = 1;
+                }
+                Console.WriteLine("count: " + count + " | maxcount: " + maxcount);
+
+                prevnum = num;
+            }
+
+            if ((maxcount < count) && (count != 1)) maxcount = count;
+
+            if (maxcount == int.MinValue) Console.WriteLine("В последовательности отсутствуют " +
+                "подпоследовательности одинаковых чётных элементов");
+            else Console.WriteLine("Максимальный размер подпоследовательности, " +
+                "состоящей из одинаковых чётных элементов: " + maxcount);
+        }
+    }
+}
---
+++ Семестр 1/Лабораторные работы/Лабораторная работа 3 04_10_2024/Задание 3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3_setkaliev_04_10_2024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Определение максимальной суммы подпоследовательности,
+            // состоящей из чётных элементов,
+            // последовательности из n элементов
+            Console.Write("Определение максимальной суммы подпоследовательности, " +
+                "состоящей из чётных элементов" +
+                "\nВведите количество элементов: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            bool check = false;
+            int sum = 0;
+            int maxsum = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите элемент: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                if (num % 2 == 0)
+                {
+                    check = true;
+                    sum += num;
+                }
+                else
+                {
+                    if ((maxsum < sum) && (check)) maxsum = sum;
+                    sum = 0;
+                }
+                Console.WriteLine("sum: " + sum + " | maxsum: " + maxsum);
+            }
+
+            if ((maxsum < sum) && (check)) maxsum = sum;
+
+            if (!check) Console.WriteLine("В последовательности отсутствуют " +
+                "чётные элементы");
+            else Console.WriteLine("Максимальная сумма элементов подпоследовательности, " +
+                "состоящей из чётных элементов: " + maxsum);
+        }
+    }
+}
