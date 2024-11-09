ff2fc9b3bf5943126118392b74287c61eb07e47b
DAMIR <125278476+DamirSM@users.noreply.github.com>
2024-11-09 13:51:38 +0600

    Add files via upload

---
+++ Семестр 1/Лабораторные работы/Лабораторная работа 5 30_10_2024/Задания 1, 2, 3.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину массива: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write("Введите элемент массива: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(array[i]);
+            }
+
+            // Задача 1
+            Console.WriteLine("\nЗадача 1");
+            int count = 0;
+            for (int i = 0; i < len; i++)
+            {
+                if (Math.Abs(array[i] % 10) == 3) count++;
+                Console.WriteLine(array[i] % 10);
+            }
+            Console.WriteLine("Количество элементов, оканчивающихся на 3: " + count);
+
+            // Задача 2
+            Console.WriteLine("\nЗадача 2");
+            if (array.Length > 1)
+            {
+                int dif = array[1] - array[0];
+                bool check = true;
+                if (dif <= 0) Console.WriteLine("Элементы массива не являются "
+                        + "равномерно возрастающей последовательностью");
+                else
+                {
+                    for (int i = 2; i < len; i++)
+                    {
+                        if (dif <= 0 || array[i] - array[i - 1] != dif) check = false;
+                        dif = array[i] - array[i - 1];
+                        Console.WriteLine(dif);
+                    }
+                    if (check) Console.WriteLine("Элементы массива являются "
+                        + "равномерно возрастающей последовательностью");
+                    else Console.WriteLine("Элементы массива не являются "
+                        + "равномерно возрастающей последовательностью");
+                }
+            }
+            else Console.WriteLine("Длина массива меньше 2");
+
+            // Задача 3
+            Console.WriteLine("\nЗадача 3");
+            int imax = 0;
+            int imin = 0;
+            for (int i = 0; i < len; i++)
+            {
+                if (array[i] > array[imax]) imax = i;
+                if (array[i] < array[imin]) imin = i;
+                Console.WriteLine(array[imax] + " " + array[imin]);
+            }
+
+            Console.WriteLine("\n");
+            int[] array_new = new int[len];
+            for (int i = 0; i < len; i++)
+            {
+                array_new[i] = array[i];
+                if (i == imax) array_new[i] = array[imin];
+                if (i == imin) array_new[i] = array[imax];
+                Console.WriteLine(array_new[i] + " " + array[i]);
+            }
+        }
+    }
+}
---
+++ Семестр 1/Лабораторные работы/Лабораторная работа 6 01_11_2024/Задания 1, 2, 3.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество строк: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество столбцов: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[rows, columns];
+            int[,] array_trans = new int[columns, rows];
+            //int rows = array.GetUpperBound(0) + 1;
+            //int columns = array.Length / rows;
+
+            bool check = true;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write("Введите элемент: ");
+                    array[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nМатрица");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                    Console.Write(array[i, j] + " ");
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                    array_trans[j, i] = array[i, j];
+
+            Console.WriteLine("\nТранспонированная матрица");
+            for (int j = 0; j < columns; j++)
+            {
+                for (int i = 0; i < rows; i++)
+                    Console.Write(array_trans[j, i] + " ");
+                Console.WriteLine();
+            }
+
+            // Задача 1 Необходимо определить пары номеров столбцов,
+            // состоящих из одинаковых элементов
+
+            for (int j = 0; j < columns; j++)
+                for (int i1 = 0; i1 < rows - 1; i1++)
+                    for (int i2 = i1 + 1; i2 < rows; i2++)
+                        if (array_trans[j, i1] > array_trans[j, i2])
+                        {
+                            int temp = array_trans[j, i1];
+                            array_trans[j, i1] = array_trans[j, i2];
+                            array_trans[j, i2] = temp;
+                        }
+
+            Console.WriteLine("\nОтсортированная транспонированная матрица");
+            for (int j = 0; j < columns; j++)
+            {
+                for (int i = 0; i < rows; i++)
+                    Console.Write(array_trans[j, i] + " ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nСравнение строк транспонированной матрицы");
+            for (int j1 = 0; j1 < columns - 1; j1++)
+            {
+                Console.WriteLine($"{j1} столбец");
+                for (int j2 = j1 + 1; j2 < columns; j2++)
+                {
+                    check = true;
+                    Console.WriteLine($"|{j2} столбец сравнения|");
+                    for (int i = 0; i < rows; i++)
+                    {
+                        Console.WriteLine($"||{i} строка||");
+                        Console.WriteLine($"{array_trans[j1, i]} {array_trans[j2, i]}\n");
+                        if (array_trans[j1, i] != array_trans[j2, i])
+                        {
+                            check = false;
+                        }
+                    }
+                    if (check) Console.WriteLine($"Пара номеров столбцов, " +
+                        $"состоящих из одинаковых элементов: ({j1}, {j2})");
+                }
+            }
+
+            // Задача 2 Необходимо отсортировать строки
+            // по убыванию количества нулевых элементов в строках
+            for (int i = 0; i < rows - 1; i++)
+                for (int j = 0; j < rows - i - 1; j++)
+                {
+                    int zeroCount1 = 0;
+                    int zeroCount2 = 0;
+
+                    for (int k = 0; k < columns; k++)
+                    {
+                        if (array[j, k] == 0)
+                            zeroCount1++;
+                        if (array[j + 1, k] == 0)
+                            zeroCount2++;
+                    }
+
+                    if (zeroCount1 < zeroCount2)
+                    {
+                        for (int k = 0; k < columns; k++)
+                        {
+                            int temp = array[j, k];
+                            array[j, k] = array[j + 1, k];
+                            array[j + 1, k] = temp;
+                        }
+                    }
+                }
+
+            Console.WriteLine("\nОтсортированная по нулям матрица");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                    Console.Write(array[i, j] + " ");
+                Console.WriteLine();
+            }
+
+            // Задача 3 Необходимо в массиве поменять местами
+            // максимальный и минимальный элемент массива
+            // (максимум и минимум - единственны)
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            int max_i = 0;
+            int max_j = 0;
+            int min_i = 0;
+            int min_j = 0;
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                {
+                    if (array[i, j] > max)
+                    {
+                        max = array[i, j];
+                        max_i = i;
+                        max_j = j;
+                    }
+                    if (array[i, j] < min)
+                    {
+                        min = array[i, j];
+                        min_i = i;
+                        min_j = j;
+                    }
+                }
+            
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                {
+                    if (i == max_i && j == max_j) array[i, j] = min;
+                    if (i == min_i && j == min_j) array[i, j] = max;
+                }
+
+            Console.WriteLine("\nМатрица, с замёнными " +
+                "максимальным и минимальным значениями");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                    Console.Write(array[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
