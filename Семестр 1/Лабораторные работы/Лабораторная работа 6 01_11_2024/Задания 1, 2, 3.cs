39c9d7d2d1eba592384f96b07488723287fa031e
DAMIR <125278476+DamirSM@users.noreply.github.com>
2024-12-11 19:17:56 +0600

    Исправлена Задача 1
    Изменён способ проверки совпадения состава столбцов матрицы в Задаче 1 на сравнение суммы, произведения и количества нулей в столбцах.

--- Семестр 1/Лабораторные работы/Лабораторная работа 6 01_11_2024/Задания 1, 2, 3.cs
+++ Семестр 1/Лабораторные работы/Лабораторная работа 6 01_11_2024/Задания 1, 2, 3.cs
@@ -15,11 +15,11 @@ namespace ConsoleApplication1
             Console.Write("Введите количество столбцов: ");
             int columns = Convert.ToInt32(Console.ReadLine());
             int[,] array = new int[rows, columns];
-            int[,] array_trans = new int[columns, rows];
+            int[,] stats = new int[columns, 3];
+            
+            //int[,] array_trans = new int[columns, rows];
             //int rows = array.GetUpperBound(0) + 1;
             //int columns = array.Length / rows;
-
-            bool check = true;
 
             for (int i = 0; i < rows; i++)
             {
@@ -40,40 +40,50 @@ namespace ConsoleApplication1
                 Console.WriteLine();
             }
 
-            for (int i = 0; i < rows; i++)
-                for (int j = 0; j < columns; j++)
-                    array_trans[j, i] = array[i, j];
+            //for (int i = 0; i < rows; i++)
+            //    for (int j = 0; j < columns; j++)
+            //        array_trans[j, i] = array[i, j];
 
-            Console.WriteLine("\nТранспонированная матрица");
-            for (int j = 0; j < columns; j++)
-            {
-                for (int i = 0; i < rows; i++)
-                    Console.Write(array_trans[j, i] + " ");
-                Console.WriteLine();
-            }
+            //Console.WriteLine("\nТранспонированная матрица");
+            //for (int j = 0; j < columns; j++)
+            //{
+            //    for (int i = 0; i < rows; i++)
+            //        Console.Write(array_trans[j, i] + " ");
+            //    Console.WriteLine();
+            //}
 
+            
             // Задача 1 Необходимо определить пары номеров столбцов,
             // состоящих из одинаковых элементов
 
-            for (int j = 0; j < columns; j++)
-                for (int i1 = 0; i1 < rows - 1; i1++)
-                    for (int i2 = i1 + 1; i2 < rows; i2++)
-                        if (array_trans[j, i1] > array_trans[j, i2])
-                        {
-                            int temp = array_trans[j, i1];
-                            array_trans[j, i1] = array_trans[j, i2];
-                            array_trans[j, i2] = temp;
-                        }
-
-            Console.WriteLine("\nОтсортированная транспонированная матрица");
+            bool check = true;
+            
             for (int j = 0; j < columns; j++)
             {
+                Console.WriteLine($"\n{j} столбец");
+                int sum = 0;
+                int mult = 1;
+                int zeroCount = 0;
                 for (int i = 0; i < rows; i++)
-                    Console.Write(array_trans[j, i] + " ");
-                Console.WriteLine();
+                {
+                    if (array[i, j] == 0)
+                        zeroCount += 1;
+                    else
+                    {
+                        sum += array[i, j];
+                        mult *= array[i, j];
+                    }
+                }
+                stats[j, 0] = sum;
+                stats[j, 1] = mult;
+                stats[j, 2] = zeroCount;
+                Console.WriteLine($"Сумма элементов столбца:\t{stats[j, 0]}\n" +
+                    $"Произведение элементов столбца:\t{stats[j, 1]}\n" +
+                    $"Количество нулей в столбце:\t{stats[j, 2]}\n");
             }
 
-            Console.WriteLine("\nСравнение строк транспонированной матрицы");
+            Console.WriteLine("\nСравнение суммы, произведения и количества " +
+                "нулей столбцов матрицы");
             for (int j1 = 0; j1 < columns - 1; j1++)
             {
                 Console.WriteLine($"{j1} столбец");
@@ -81,22 +91,61 @@ namespace ConsoleApplication1
                 {
                     check = true;
                     Console.WriteLine($"|{j2} столбец сравнения|");
-                    for (int i = 0; i < rows; i++)
-                    {
-                        Console.WriteLine($"||{i} строка||");
-                        Console.WriteLine($"{array_trans[j1, i]} {array_trans[j2, i]}\n");
-                        if (array_trans[j1, i] != array_trans[j2, i])
+                    for (int i = 0; i < 3; i++)
+                        if (stats[j1, i] != stats[j2, i])
                         {
                             check = false;
+                            break;
                         }
-                    }
                     if (check) Console.WriteLine($"Пара номеров столбцов, " +
                         $"состоящих из одинаковых элементов: ({j1}, {j2})");
                 }
             }
+            
+            //for (int j = 0; j < columns; j++)
+            //    for (int i1 = 0; i1 < rows - 1; i1++)
+            //        for (int i2 = i1 + 1; i2 < rows; i2++)
+            //           if (array_trans[j, i1] > array_trans[j, i2])
+            //            {
+            //                int temp = array_trans[j, i1];
+            //                array_trans[j, i1] = array_trans[j, i2];
+            //                array_trans[j, i2] = temp;
+            //            }
 
+            //Console.WriteLine("\nОтсортированная транспонированная матрица");
+            //for (int j = 0; j < columns; j++)
+            //{
+            //    for (int i = 0; i < rows; i++)
+            //        Console.Write(array_trans[j, i] + " ");
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine("\nСравнение строк транспонированной матрицы");
+            //for (int j1 = 0; j1 < columns - 1; j1++)
+            //{
+            //    Console.WriteLine($"{j1} столбец");
+            //    for (int j2 = j1 + 1; j2 < columns; j2++)
+            //    {
+            //        check = true;
+            //        Console.WriteLine($"|{j2} столбец сравнения|");
+            //        for (int i = 0; i < rows; i++)
+            //        {
+            //            Console.WriteLine($"||{i} строка||");
+            //            Console.WriteLine($"{array_trans[j1, i]} {array_trans[j2, i]}\n");
+            //            if (array_trans[j1, i] != array_trans[j2, i])
+            //           {
+            //                check = false;
+            //            }
+            //        }
+            //        if (check) Console.WriteLine($"Пара номеров столбцов, " +
+            //           $"состоящих из одинаковых элементов: ({j1}, {j2})");
+            //    }
+            //}
+
+            
             // Задача 2 Необходимо отсортировать строки
             // по убыванию количества нулевых элементов в строках
+            
             for (int i = 0; i < rows - 1; i++)
                 for (int j = 0; j < rows - i - 1; j++)
                 {
@@ -130,9 +179,11 @@ namespace ConsoleApplication1
                 Console.WriteLine();
             }
 
+            
             // Задача 3 Необходимо в массиве поменять местами
             // максимальный и минимальный элемент массива
             // (максимум и минимум - единственны)
+            
             int max = int.MinValue;
             int min = int.MaxValue;
             int max_i = 0;
