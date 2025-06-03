f1c031760d0063efdbef0ace7000328dcce33081
DAMIR <125278476+DamirSM@users.noreply.github.com>
2025-06-03 17:28:03 +0600

    Add files via upload

---
+++ Семестр 2/Алгоритмизация и Программирование/Лабораторные работы/Лабораторная работа 5 12_03_2025/Задача 1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Phone
+    {
+        public int number { get; set; }
+        public string mobile_oper { get; set; }
+        
+        public Phone (int number, string mobile_oper)
+        {
+            this.number = number;
+            this.mobile_oper = mobile_oper;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Phone> phones = new List<Phone>();
+            while(true)
+            {
+                Console.Write("Номер телефона: ");
+                string temp = Console.ReadLine();
+                if (temp == "")
+                    break;
+                int number = Convert.ToInt32(temp);
+                Console.Write("Оператор связи: ");
+                string mobile_oper = Console.ReadLine();
+                if (mobile_oper == "")
+                    break;
+                Phone phone = new Phone(number, mobile_oper);
+                phones.Add(phone);
+		Console.WriteLine();
+            }
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (Phone phone in phones)
+            {
+                string key = phone.mobile_oper;
+                if (dict.ContainsKey(key))
+                {
+                    dict[key]++;
+                }
+                else
+                    dict.Add(key, 1);
+            }
+
+            foreach (string key in dict.Keys)
+            {
+                Console.WriteLine(key + ": " + dict[key]);
+            }
+        }
+    }
+}
---
+++ Семестр 2/Алгоритмизация и Программирование/Лабораторные работы/Лабораторная работа 5 12_03_2025/Задача 2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    delegate void Eval(ref int input);
+
+    class Equation
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+
+        public Equation(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int Sum(int x1, int x2)
+        {
+            return (x1+x2);
+        }
+
+        public int Dif(int x1, int x2)
+        {
+            return (x1 - x2);
+        }
+
+        public int Mult(int x1, int x2)
+        {
+            return(x1*x2);
+        }
+
+        public int Div(int x1, int x2)
+        {
+            if (x2 == 0)
+            {
+                Console.WriteLine("Деление на ноль невозможно");
+                throw new DivideByZeroException();
+            }
+            return(x1/x2);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation eq = new Equation(10, 5);
+
+            Eval op1 = null;
+            op1 += (ref int result) =>
+            { 
+                result = eq.Sum(eq.a, eq.b);
+            };
+
+            op1 += (ref int result) =>
+            {
+                result = eq.Dif(result, eq.b);
+            };
+
+            op1 += (ref int result) =>
+            {
+                result = eq.Mult(result, eq.a);
+            };
+
+            int result1 = 0;
+            op1(ref result1);
+
+            Console.WriteLine(result1);
+
+            Eval op2 = null;
+            op2 += (ref int result) =>
+            {
+                result = eq.Mult(eq.a, eq.b);
+            };
+
+            op2 += (ref int result) =>
+            {
+                result = eq.Dif(result, eq.a);
+            };
+
+            op2 += (ref int result) =>
+            {
+                result = eq.Div(result, eq.a);
+            };
+
+            int result2 = 0;
+            op2(ref result2);
+
+            Console.WriteLine(result2);
+        }
+    }
+}
